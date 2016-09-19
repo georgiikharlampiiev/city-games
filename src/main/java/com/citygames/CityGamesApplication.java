38a6e8a513b70bacfd099fafd71008906fe82ece
ArteZious <artezious@icloud.com>
2016-09-19 17:38:39 +0300

    Commit #2 cleaning)

--- src/main/java/com/citygames/CityGamesApplication.java
+++ src/main/java/com/citygames/CityGamesApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CityGamesApplication {
 
-	private String test;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CityGamesApplication.class, args);
 	}
