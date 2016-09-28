c43d39ec0f1ec0280ec2ce67ddc5e3e64e95cf60
Admin <a>
2016-09-28 14:45:26 +0300

    props

---
+++ src/main/resources/static/props/Employee.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Router, hashHistory, Link} from "react-router";
+import {UpdateDialog} from '../props/UpdateDialog';
+
+export class Employee extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.employee);
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.employee.entity.firstName}</td>
+                <td>{this.props.employee.entity.lastName}</td>
+                <td>{this.props.employee.entity.description}</td>
+                <td>{this.props.employee.entity.manager.name}</td>
+                <td>
+                    <UpdateDialog employee={this.props.employee}
+                                  attributes={this.props.attributes}
+                                  onUpdate={this.props.onUpdate}/>
+                </td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
+            </tr>
+        )
+    }
+}
