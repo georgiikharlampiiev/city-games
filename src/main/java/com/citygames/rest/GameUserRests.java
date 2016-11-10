3845b8b0ad9ea5640d5f6783ae0afc65b0a8e65c
Georgii Kharlampiiev <gukulkan@gmail.com>
2016-11-10 15:39:34 +0200

    my profile update

--- src/main/java/com/citygames/rest/GameUserRests.java
+++ src/main/java/com/citygames/rest/GameUserRests.java
@@ -1,13 +1,15 @@
 package com.citygames.rest;
 
 
+import com.citygames.dto.UserDTO;
 import com.citygames.entity.GameUser;
-import com.citygames.entity.Team;
 import com.citygames.service.GameUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +25,23 @@ public class GameUserRests {
         String name = auth.getName();
 
         return gameUserService.getByName(name);
+    }
+
+    @RequestMapping(value="/setUserProfile", method= RequestMethod.POST)
+    public GameUser setGameUser(@RequestBody UserDTO gameUserDto) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        GameUser currentUser = gameUserService.getByName(name);
+
+        if( currentUser.getId().equals(gameUserDto.getId()) ){
+            currentUser.setName(gameUserDto.getName());
+            if( gameUserDto.getPassword() != null ) {
+                currentUser.setPassword( gameUserDto.getPassword() );
+            }
+            return gameUserService.edit(currentUser);
+        }
+
+        return currentUser;
     }
 
 }
