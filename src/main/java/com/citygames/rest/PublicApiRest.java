6febb0ee0ca8aa4b934335184b3713c6b66fca55
KondratiukVolodymyr <kvp1708@i.ua>
2016-11-16 14:25:54 +0200

    added ajax script

--- src/main/java/com/citygames/rest/PublicApiRest.java
+++ src/main/java/com/citygames/rest/PublicApiRest.java
@@ -1,7 +1,9 @@
 package com.citygames.rest;
 
 import com.citygames.entity.Game;
+import com.citygames.entity.GameUser;
 import com.citygames.service.GameService;
+import com.citygames.service.GameUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,16 @@ public class PublicApiRest {
     @Autowired
     private GameService gameService;
 
+    @Autowired
+    private GameUserService gameUserService;
+
     @RequestMapping("/getGames")
     public List<Game> getGamesOnIndex() {
         return gameService.getAllActiveGames(0, 10);
+    }
+
+    @RequestMapping("/registerUser")
+    public GameUser registerUser(GameUser gameUser) {
+        return gameUserService.add(gameUser);
     }
 }
